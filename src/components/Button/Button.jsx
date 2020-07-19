7236a8fb30d45083e675ff8b31a6ef95ac73b4d3
Francisco Santos <fwfs1985@gmail.com>
2020-07-19 14:33:05 -0300

    create Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 180px;
+  height: 40px;
+  background-color: #2b2b;
+  color: #fff;
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+Button.displayName = "Button";
+
+export default Button;
