dcfc0e221e93c1bf01a465b2feb5b45974169df7
Francisco Santos <fwfs1985@gmail.com>
2020-07-19 14:32:54 -0300

    create Headings

---
+++ src/components/Headings/Headings.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  margin-top: 24px;
+  margin-bottom: 24px;
+`;
+
+export const H2 = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 16px;
+  margin-bottom: 16px;
+`;
+
+export const H3 = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 12px;
+  margin-bottom: 12px;
+`;
