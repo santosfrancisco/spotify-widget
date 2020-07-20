eac7d655c844b9c13506c4d0ab7c294d5acc1391
Francisco Santos <fwfs1985@gmail.com>
2020-07-19 23:02:47 -0300

    put mongodb secrets on .env

--- src/utils/db-connect.js
+++ src/utils/db-connect.js
@@ -1,22 +1,22 @@
 /* This is a database connection function*/
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const connection = {} /* creating connection object*/
+const connection = {}; /* creating connection object*/
 
 async function dbConnect() {
   /* check if we have connection to our databse*/
   if (connection.isConnected) {
-    return
+    return;
   }
 
   /* connecting to our database */
-  const db = await mongoose.connect('mongodb://root:root1234@ds135700.mlab.com:35700/spotify-api', {
+  const db = await mongoose.connect(`${process.env.DB_CONNECTION_STRING}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-  })
+  });
 
-  connection.isConnected = db.connections[0].readyState
+  connection.isConnected = db.connections[0].readyState;
 }
 
-export default dbConnect
+export default dbConnect;
