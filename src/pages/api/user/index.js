bdc5fb998ea711aa6f10b3d079415bf0345290e8
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2021-09-16 16:10:16 -0500

    Fix issues with duplicated users in database (#19)
    * Fixed issues with users creation in database
    
    If a user registers, it will be checked if there is one, several or none and then it will be updated in the database
    
    * Added re-login button
    
    Added re login button to avoid problems when the user tries to log in again if permissions were removed from Spotify apps

--- src/pages/api/user/index.js
+++ src/pages/api/user/index.js
@@ -4,7 +4,7 @@ import { useCors } from '../../../middlewares/cors'
 
 export default async function handler(req, res) {
   await useCors(req, res)
-  
+
   const {
     method,
   } = req
@@ -12,11 +12,21 @@ export default async function handler(req, res) {
   await dbConnect()
 
   switch (method) {
-
     case 'POST':
       try {
-        const user = await User.create(req.body) 
-        res.status(201).json({ success: true, data: user })
+        await User.find({ user: req.body.user }, async (err, users) => {
+          if (!err && users.length > 1) {
+            await User.deleteMany({ user: req.body.user })
+            const user = await User.create(req.body)
+            res.status(201).json({ success: true, data: user })
+          } else if (!err && users.length == 1) {
+            const user = await User.findOneAndUpdate({user: req.body.user}, req.body)
+            res.status(201).json({ success: true, data: user })
+          } else if (!err && users.length == 0) {
+            const user = await User.create(req.body)
+            res.status(201).json({ success: true, data: user })
+          }
+        });
       } catch (error) {
         res.status(400).json({ success: false })
       }
